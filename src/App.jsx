72a7d0bd4fd6ffc2a032de3845cf4de426f0b164
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-06-08 14:54:40 -0500

    Implement memory saving functionality and enhance styling for textarea and button

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,51 @@ import { useState } from 'react';
 
 function App() {
   const [entry, setEntry] = useState('');
-  /*const [memories, setMemories] = useState([]);*/
+  const [memories, setMemories] = useState([]);
 
+  function saveEntry() {
+    if (entry.trim() === '') return;
+
+    const newMemory = {
+      id: Date.now(),
+      text: entry,
+      x: Math.random() * 90,
+      y: Math.random() * 70,
+    };
+    setMemories([...memories, newMemory]);
+    setEntry('');
+  }
   return (
     <div>
       <h1>Luminary</h1>
-      <p>Your inner life as a night sky.</p>
+      <p>Your inner life as a night sky</p>
 
-      <textarea 
-        placeholder="Write a thought, memory, or gratitude here..."
-        value = {entry}
+      <textarea
+        placeholder="Write a thought, memory, or gratitude..."
+        value={entry}
         onChange={(event) => setEntry(event.target.value)}
-      />
+        />
+      <br />
 
-      <p>You wrote: {entry}</p>
+      <button onClick={saveEntry}>Plant Star</button>
+      <h2>Memories</h2>
+      
+      <div className="Sky">
+        {memories.map((memory) => (
+          <div
+          key={memory.id}
+          className="star"
+          style={{
+            left: `${memory.x}%`,
+            right: `${memory.y}%`
+          }}
+          title={memory.text}
+          />
+        ))}
+      </div>
+
     </div>
-  )
-
+  );
 }
 
 export default App;
-
- 
