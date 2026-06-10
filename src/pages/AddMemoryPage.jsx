04c98d1f2992ae3adb589b0ffd911d53a9d6e065
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-06-10 12:50:01 -0500

    Added emotion detection baseeed on keywords in the text. Added a new page that will allow user to write a memory and save it.

---
+++ src/pages/AddMemoryPage.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+
+function AddMemoryPage({ addMemory }) {
+    const [entry, setEntry] = useState('');
+
+    function detectEmotion(text) {
+        const lowerText = text.toLowerCase();
+
+        if (
+            lowerText.includes("happy") ||
+            lowerText.includes("joy") ||
+            lowerText.includes("excited") ||
+            lowerText.includes("love")
+        ) {
+            return 'happy';
+        }
+
+        if (
+            lowerText.includes('sad') ||
+            lowerText.includes("depressed") ||
+            lowerText.includes("unhappy") ||
+            lowerText.includes("miserable") ||
+            lowerText.includes("miss")
+        ) {
+            return 'sadness';
+        }
+
+        if (
+            lowerText.includes('angry') ||
+            lowerText.includes("mad") ||
+            lowerText.includes("frustrated") ||
+            lowerText.includes("annoyed") ||
+            lowerText.includes("hate")
+        ) {
+            return 'anger';
+        }
+        
+        return 'calm';
+
+    }
+
+    function saveEntry() {
+        if {entry.trim() === ''} return;
+
+        const NewMemory = {
+            id: Date.now(),
+            text: entry,
+            emotion: detectEmotion(entry)
+            x: Math.random() * 100,
+            y: Math.random() * 100,
+            createdAt: new Date().toISOString()
+        };
+
+        addMemory(NewMemory);
+        setEntry('');
+    }
+
+    return (
+        <main>
+            <section className="page add-page">
+                <h1> Plant a Memory </h1>
+                <p>
+                    Write a thought like you would in a diary.
+                    Luminary will turn it into star in your sky.
+                </p>
+
+                <textarea
+                    placeholder="Write your memory here..."
+                    value={entry}
+                    onChange={(e) => setEntry(e.target.value)}
+                />
+            
+                <button onClick={saveEntry} className="primary-button">
+                    Plant Star
+                </button>
+            </section>
+            
+        </main>
+    );
+
+}
